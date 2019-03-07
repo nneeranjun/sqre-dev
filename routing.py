d6300fb4f820e35d861afaad774be98cc98482e3
Nilay Neeranjun <nneeranjun@berkeley.edu>
2019-03-07 03:14:01 -0800

    Got register and getUser working

--- routing.py
+++ routing.py
@@ -1,14 +1,27 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask import request
+from bson.objectid import ObjectId
+from flask import jsonify
+from user import User
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/usersdb"
 mongo = PyMongo(app)
 
 @app.route('/register', methods = ['POST'])
 def register():
-   return 'Hello World'
+        json_data = request.get_json(force=True) 
+        mongo.db.users.insert(json_data)
+        return json_data["_id"]
 @app.route('/')
-def hello():
-    for x in mongo.db.users.find({"name": "Nilay"}):
-        return str(x)
+def index():
+        return "index"
+
+@app.route('/users/<user_id>', methods = ['POST', 'GET', 'DELETE'])
+def getUser(user_id):
+        if request.method == 'GET':
+                for x in mongo.db.users.find({"_id": ObjectId(user_id)}):
+                        user = User(x.get("name"), x.get("email"), x.get("phone_number"), user_id)
+                        return jsonify(user.serialize())
+                        
--- user.py
+++ user.py
@@ -1,5 +1,15 @@
 class User:
-    def __init__(self, name, email, phone_number):
+    def __init__(self, name, email, phone_number, user_id):
         self.name = name
         self.email = email
         self.phone_number = phone_number
+        self.user_id = user_id
+    def __str__(self):
+        return "" + self.name + " " + self.email + " " + self.phone_number
+    def serialize(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "user_id": self.user_id
+        }
