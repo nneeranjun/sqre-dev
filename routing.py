6e391509f57376e4455839946c0c1318e3c0fb36
Nilay Neeranjun <nneeranjun@berkeley.edu>
2019-03-06 22:57:06 -0800

    Database is connected

--- routing.py
+++ routing.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask import request
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/usersdb"
+mongo = PyMongo(app)
 
 @app.route('/register', methods = ['POST'])
 def register():
    return 'Hello World'
 @app.route('/')
 def hello():
-    return 'Hello'
+    for x in mongo.db.users.find({"name": "Nilay"}):
+        return str(x)
