6361135c9f87b49bc00538bee0a0a4b351dd01fc
Nilay Neeranjun <nneeranjun@berkeley.edu>
2019-03-26 03:04:52 -0400

    Working on qr scanner

---
+++ sQRe/QrScanner.swift
@@ -0,0 +1,55 @@
+//
+//  QrScanner.swift
+//  sQRe
+//
+//  Created by Nilay Neeranjun on 3/26/19.
+//  Copyright © 2019 Nilay Neeranjun. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QrScanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var video = AVCaptureVideoPreviewLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //creating session
+        let session = AVCaptureSession()
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        } catch {
+            print("error")
+        }
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        session.startRunning()
+        // Do any additional setup after loading the view.
+    }
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != nil {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+                
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
